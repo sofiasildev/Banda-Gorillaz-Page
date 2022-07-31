fb96a30fb8a651dc4e7b210e84460b316241c7e7
sofiasildev <silvaheviasofia@gmail.com>
2022-07-30 22:50:15 -0300

    modificaciones generales

--- css/styles.c
+++ css/styles.c
@@ -1,18 +1,86 @@
-/* Error: Undefined variable.
- *    ,
- * 13 |     $palette
- *    |     ^^^^^^^^
- *    '
- *   scss/variables.scss 13:5  @import
- *   scss/styles.scss 6:9      root stylesheet */
-
-body::before {
-  font-family: "Source Code Pro", "SF Mono", Monaco, Inconsolata, "Fira Mono",
-      "Droid Sans Mono", monospace, monospace;
-  white-space: pre;
-  display: block;
-  padding: 1em;
-  margin-bottom: 1em;
-  border-bottom: 2px solid black;
-  content: "Error: Undefined variable.\a    \2577 \a 13 \2502      $palette\d\a    \2502      ^^^^^^^^\a    \2575 \a   scss/variables.scss 13:5  @import\a   scss/styles.scss 6:9      root stylesheet";
+header nav ul li {
+  height: fit-content;
 }
+
+body .background {
+  background-size: 100%;
+  background-image: url("../media/gorillaz.png");
+  background-repeat: repeat-y;
+}
+
+.background__other {
+  background: black;
+}
+
+/*aspectos de la lista*/
+.title.section__artist.section__music {
+  color: aliceblue;
+}
+
+.card-img-top {
+  width: 300px;
+  height: 350px;
+}
+
+h2 {
+  color: aliceblue;
+  padding: 10px;
+}
+
+/*modificaciones en el texto*/
+.event__texto {
+  font-size: x-large;
+  writing-mode: horizontal-tb;
+  padding: 2%;
+}
+
+.contactus__texto {
+  font-size: x-large;
+}
+
+#contactus {
+  color: aliceblue;
+}
+
+#logos {
+  background-color: aliceblue;
+  width: 50px;
+  height: 50px;
+}
+
+.contactus__texto {
+  font-size: larger;
+  width: 40%;
+  color: bisque;
+}
+
+.card-body {
+  color: black;
+}
+
+.row {
+  padding: 20px;
+  margin: 10px;
+}
+
+/*page social*/
+.redes {
+  width: 20px;
+}
+
+/*grid y flex store*/
+img {
+  width: 300px;
+  height: auto;
+  margin: auto;
+  padding: 15px;
+}
+
+/*MEDIA QUERIES*/
+@media screen and (min-width: 600px) {
+  html {
+    font-size: 14px;
+  }
+}
+
+/*# sourceMappingURL=styles.c.map */
